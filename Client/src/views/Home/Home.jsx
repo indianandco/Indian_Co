fb4aef8de799f0a6917dfe4c097bcb42c2cb7d65
Juan <juampi.parzybal@gmail.com>
2023-07-27 11:54:36 -0300

    arrancando front

---
+++ Client/src/views/Home/Home.jsx
@@ -0,0 +1,11 @@
+import "./Home.css"
+
+const Home = () => {
+    return (
+        <div className="container" >
+            <h1>Hola mundo! este es el Home</h1>
+        </div>
+    )
+}
+
+export default Home;
