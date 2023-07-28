7df7ee25e1f975968133301743dc3cedf34a7a63
Juan <juampi.parzybal@gmail.com>
2023-07-28 16:24:09 -0300

    implementando funcionalidad a la searchbar y contexto global de react

--- Client/src/components/SearchBar/SearchBar.jsx
+++ Client/src/components/SearchBar/SearchBar.jsx
@@ -1,18 +1,37 @@
 import "./SearchBar.css"
 import Form from 'react-bootstrap/Form';
 import InputGroup from 'react-bootstrap/InputGroup';
-
+import Button from 'react-bootstrap/Button';
+import { SearchContext } from "../../services/SearchContext";
+import { useContext, useState } from "react";
 
 const SearchBar = () => {
+
+    const [search, setSearch] = useState();
+    const { findProduct } = useContext(SearchContext);
+
+    const handleSearch = (search) => {
+        findProduct(search)
+    }
+
+    const handleChange = (event) => {
+        setSearch(event.target.value)
+    }
+
     return (
-        <InputGroup size="sm" className="mb-1">
-            <InputGroup.Text id="inputGroup-sizing-sm">Buscar</InputGroup.Text>
-            <Form.Control
-                placeholder="¿Qué estás buscando?"
-                aria-label="small"
-                aria-describedby="inputGroup-sizing-sm"
-            />
-        </InputGroup>
+        <div className="search_container">
+            <InputGroup className="mb-3">
+                <Form.Control
+                    onChange={handleChange}
+                    placeholder="¿Qué estás buscando?"
+                    aria-label="¿Qué estás buscando?"
+                    aria-describedby="basic-addon2"
+                />
+                <Button onClick={() => handleSearch(search)} variant="info" id="button-addon2">
+                    Buscar
+                </Button>
+            </InputGroup>
+        </div>
     );
 }
 
