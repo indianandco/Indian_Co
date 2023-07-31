de386d8f2b916c44c3a9abd7a4abaac1411f303b
Juan <juampi.parzybal@gmail.com>
2023-07-31 16:22:48 -0300

    agregando funcionalidad y modal a la searchbar

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,7 +1,6 @@
 import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Routes, Route } from "react-router-dom"
-import Landing from './views/Landing/Landing';
 import Home from './views/Home/Home';
 import Navbar from "./components/Navbar/Navbar"
 import CreateProduct from './components/CreateProduct/CreateProduct';
@@ -19,8 +18,7 @@ function App() {
       <Navbar />
 
       <Routes>
-        <Route path='/' element={<Landing />} />
-        <Route path='/home' element={<Home />} />
+        <Route path='/' element={<Home />} />
         <Route path='/about' element={<Home />} />
         <Route path='/contact' element={<Contact />} />
         <Route path='/createProduct' element={<CreateProduct />} />
--- Client/src/utils/fetcher.js
+++ Client/src/utils/fetcher.js
@@ -1,17 +1,18 @@
 import axios from "axios";
 
-const BASE_URL = "http://localhost:3001/"; // Reemplaza esto con la URL de tu API
+const BASE_URL = "http://localhost:3001"; // Reemplaza esto con la URL de tu API
 
 export const fetcher = async (endpoint) => {
   try {
-    const response = await axios(`${BASE_URL}${endpoint}`);
+    const response = await axios.get(`${BASE_URL}/${endpoint}`);
+    console.log(response);
 
-    if (!response.ok) {
+    if (!response.statusText === "OK") {
       throw new Error('Network response was not ok');
     }
 
-    const data = await response.json();
-    return data;
+    return response.data
+
   } catch (error) {
     console.error('Error fetching data:', error);
     throw error;
