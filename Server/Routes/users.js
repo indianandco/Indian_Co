23aac6f35552ee28802f1d130b92c727ba95a6f9
Federico Paglia <federicoepaglia@gmail.com>
2023-08-03 16:16:03 -0300

    Login con fb y google
    Faltan las credenciales

--- Server/Routes/users.js
+++ Server/Routes/users.js
@@ -7,6 +7,7 @@ const { failRegister } = require('../Handlers/get/failRegister');
 const { loginHandler } = require('../Handlers/post/loginHandler');
 const { failLogin } = require('../Handlers/get/failLogin');
 const { logOut } = require('../Handlers/get/logOut');
+const { callBackAuthenticate } = require('../Handlers/get/callBackAuthenticate');
 
 
 const { getUserByNameHandler } = require ('../Handlers/get/getUserByNameHandler')
@@ -20,6 +21,14 @@ router.get('/fail-register', failRegister);
 //Login:
 router.post('/login', passport.authenticate('login', {failureRedirect: 'fail-login'}), loginHandler);
 router.get('/fail-login', failLogin );
+
+//Login/register con Facebook:
+router.get('/auth/facebook', passport.authenticate('facebook'), postUsersHandler);
+router.get('/auth/facebook/callback', passport.authenticate('facebook', { failureRedirect: '/login' }), callBackAuthenticate);
+//Login/register con Google:
+router.get('/auth/google', passport.authenticate('google', { scope: ['profile'] }), loginHandler);
+router.get('/auth/google/callback', passport.authenticate('google', { failureRedirect: '/login' }), callBackAuthenticate);
+
 //Logout:
 router.get('/logout', logOut);
 
