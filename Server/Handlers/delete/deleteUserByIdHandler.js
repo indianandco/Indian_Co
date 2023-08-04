05c991f61f56766e87e427032594c6c4a6ca1b60
Federico Paglia <federicoepaglia@gmail.com>
2023-08-04 11:44:07 -0300

    feature/DashboardAdminRoutes

--- Server/Handlers/delete/deleteUserByIdHandler.js
+++ Server/Handlers/delete/deleteUserByIdHandler.js
@@ -1,15 +1,16 @@
-const { deleteUserByIdController } = require('../../Controllers/delete/deleteUserByIdController')
+const { deleteUserByIdController } = require('../../Controllers/delete/deleteUserByIdController');
 
 const deleteUserByIdHandler = async (req, res)=>{
-try {
-    const { id } = req.params
-    const deleteUser= await deleteUserByIdController(id)
-    return res.status(200).json({result:'success', payload:deleteUser})
+    try {
+        const { id } = req.params;
+        const deleteUser= await deleteUserByIdController(id);
+        
+        return res.status(200).json({result:'success', payload:deleteUser});
 
-} catch (error) {
-    res.status(400).json({error:error.message})
-}
-}
+    } catch (error) {
+        res.status(400).json({error:error.message});
+    };
+};
 
 module.exports={
     deleteUserByIdHandler
