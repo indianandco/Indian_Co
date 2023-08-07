29085bb18cdc6b850cde94dfbaa95ff06d2672ac
Juan <juampi.parzybal@gmail.com>
2023-08-07 13:14:40 -0300

    arreglando merge

--- Client/src/views/DashboardAdmin/DashboardAdmin.jsx
+++ Client/src/views/DashboardAdmin/DashboardAdmin.jsx
@@ -18,22 +18,6 @@ const DashboardAdmin = () => {
 
     return (
         <div>
-            <div className="dash_container">
-                <div className="offcanvas_container">
-                    <Button variant="primary" onClick={handleShow} style={{ width: "100%" }} className="me-2 icon border-0 rounded-0">
-                        <i className="bi bi-list"></i>
-                    </Button>
-                    <Nav defaultActiveKey="/home" style={{ height: "100%" }} className=" nav_container flex-column d-flex align-items-start justify-content-start">
-                        <Nav.Link className="icon" href="/home"><i className="bi bi-house-door-fill"></i></Nav.Link>
-                        <Nav.Link className="icon" eventKey="link-1"><i className="bi bi-people"></i></Nav.Link>
-                        <Nav.Link className="icon" eventKey="link-2"><i className="bi bi-ticket"></i></Nav.Link>
-                        <Nav.Link className="icon" href="/createProduct"><i className="bi bi-upload"></i></Nav.Link>
-                    </Nav>
-                </div>
-                <div className="banners_container">
-                    <h1>Banners, etc...</h1>
-                </div>
-            </div>
             <div>
                 <Offcanvas show={show} onHide={handleClose}>
                     <Offcanvas.Header closeButton>
@@ -57,6 +41,7 @@ const DashboardAdmin = () => {
                             <Nav.Link className="icon" href="/home"><i className="bi bi-house-door-fill"></i></Nav.Link>
                             <Nav.Link className="icon" eventKey="link-1"><i className="bi bi-people"></i></Nav.Link>
                             <Nav.Link className="icon" eventKey="link-2"><i className="bi bi-ticket"></i></Nav.Link>
+                            <Nav.Link className="icon" href="/createProduct"><i className="bi bi-upload"></i></Nav.Link>
                         </Nav>
                     </div>
 
