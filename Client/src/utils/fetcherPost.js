21d745fe22aa7d450da812f9f39d0bfaf6d6169e
Juan <juampi.parzybal@gmail.com>
2023-08-09 16:36:02 -0300

    fix register form solved

--- Client/src/utils/fetcherPost.js
+++ Client/src/utils/fetcherPost.js
@@ -15,7 +15,6 @@ export const postProductFunction = async (endpoint, product)=>{
 export const fetcherCreateUser = async (endpoint, form)=>{
     try {
         const response = await axios.post(`${BASE_URL}${endpoint}`, form)
-        console.log(response);
         return response.data
     } catch (error) {
         console.error('Error fetching data:', error);
--- Server/Handlers/post/postUsersHandler.js
+++ Server/Handlers/post/postUsersHandler.js
@@ -1,6 +1,8 @@
+const { log } = require("console");
+
 const postUsersHandler = async (req, res) => {
     try {
-
+        console.log("hola mundo");
         res.status(201).send({ status: "success", message: "User registered" });
         
     } catch (error) {
