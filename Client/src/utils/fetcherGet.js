fdeb1cd7afcc1686f4d8694f20057b33f46397bc
Juan <juampi.parzybal@gmail.com>
2023-08-09 11:08:04 -0300

    funcionalidad dashboard

--- Client/src/utils/fetcherGet.js
+++ Client/src/utils/fetcherGet.js
@@ -5,11 +5,11 @@ const BASE_URL = "http://localhost:3001"; // Reemplaza esto con la URL de tu API
 export const fetcher = async (endpoint) => {
   try {
     const response = await axios.get(`${BASE_URL}${endpoint}`);
-    console.log(response);
-
+    
     if (!response.statusText === "OK") {
       throw new Error('Error en la petición');
     }
+    console.log(response.data);
     
     return response.data
 
--- Client/src/views/DashboardAdmin/AdminComponents/LineChart/LineChart.jsx
+++ Client/src/views/DashboardAdmin/AdminComponents/LineChart/LineChart.jsx
@@ -22,45 +22,47 @@ ChartJS.register(
     Filler
 );
 
-let dailySales = [0, 56, 20, 36, 80, 40, 30, 25, 30, 12, 60];
-let days = ["Lunes", "Martes", "Miércoles", "Jueves", "Viernes", "Sábado", "Domingo"]
 
-
-const weekSales = {
-    labels: days,
-    datasets: [
-        {
-            label: 'Ventas',
-            data: dailySales,
-            tension: 0.5,
-            fill: true,
-            borderColor: 'rgb(0, 0, 0, 0.589)',
-            backgroundColor: 'rgba(221, 112, 28, 0.492)',
-            pointRadius: 5,
-            pointBorderColor: 'rgba(255, 255, 255)',
-            pointBackgroundColor: 'rgba(255, 255, 255)',
-        },
-    ],
-};
-
-const options = {
-    plugins: {
-        legend: {
-            labels: {
-                color: 'black', // Cambia el color del texto de los labels aquí
-            }
-        }
-    },
-    scales: {
-        y: {
-            min: 0
-        },
-        x: {
-            ticks: { color: 'rgb(255, 255, 255)' }
-        }
-    }
-};
 
 export default function LinesChart() {
+
+    let dailySales = [0, 56, 20, 36, 80, 40, 30, 25, 30, 12, 60];
+    let days = ["Lunes", "Martes", "Miércoles", "Jueves", "Viernes", "Sábado", "Domingo"]
+
+    const weekSales = {
+        labels: days,
+        datasets: [
+            {
+                label: 'Ventas',
+                data: dailySales,
+                tension: 0.5,
+                fill: true,
+                borderColor: 'rgb(0, 0, 0, 0.589)',
+                backgroundColor: 'rgba(221, 112, 28, 0.492)',
+                pointRadius: 5,
+                pointBorderColor: 'rgba(255, 255, 255)',
+                pointBackgroundColor: 'rgba(255, 255, 255)',
+            },
+        ],
+    };
+
+    const options = {
+        plugins: {
+            legend: {
+                labels: {
+                    color: 'black', // Cambia el color del texto de los labels aquí
+                }
+            }
+        },
+        scales: {
+            y: {
+                min: 0
+            },
+            x: {
+                ticks: { color: 'rgb(255, 255, 255)' }
+            }
+        }
+    };
+
     return <Line data={weekSales} options={options} />
 }
