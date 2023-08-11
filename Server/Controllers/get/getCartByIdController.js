d702d5dad17a7c15c718cfbb41d92efe442d3736
fepaglia <105800462+fepaglia@users.noreply.github.com>
2023-08-11 15:04:30 -0300

    Revert "Fixing/cart/functions"

--- Server/Controllers/get/getCartByIdController.js
+++
@@ -1,9 +0,0 @@
-const { cartModel } = require('../../models/cart.model');
-
-const getCartByIdController = async(cid) =>{
-    return await cartModel.findOne({_id: cid});
-};
-
-module.exports = {
-    getCartByIdController
-};
--- Server/Controllers/put/putTicketController.js
+++
@@ -1,9 +0,0 @@
-const { ticketModel } = require('../../models/ticket.model');
-
-const putTicketController = async (tid, status) =>{
-    return await ticketModel.findByIdAndUpdate({_id: tid}, status);
-};
-
-module.exports = {
-    putTicketController
-}
