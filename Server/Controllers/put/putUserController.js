b25a8a9c6dfc0e41351f23129fc4a002609eb2bc
Juan <juampi.parzybal@gmail.com>
2023-08-17 11:47:28 -0300

    refactor(users): agregando hash a la ruta put para perfil de usuario

--- Server/Controllers/put/putUserController.js
+++ Server/Controllers/put/putUserController.js
@@ -1,8 +1,8 @@
 const { userModel } = require('../../models/user.model');
 
-const putUserController =  async (uid, userData) =>{
+const putUserController =  async (uid, newUserInfo) =>{
     
-    return await userModel.findByIdAndUpdate({_id: uid}, userData);
+    return await userModel.findByIdAndUpdate({_id: uid}, newUserInfo);
 };
 
 module.exports = {
