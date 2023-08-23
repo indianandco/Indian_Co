793dd988171e771a3d8369db3f59304ce0db34d6
CristianR1992 <rodriguez.cristianezequiel@hotmail.com>
2023-08-23 04:40:04 +0100

    pequeña correccion en cloudinaryConfig

--- Server/cloudinaryConfig.js
+++ Server/cloudinaryConfig.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const cloudinary = require('cloudinary').v2;
 const { API_KEY, API_SECRET, C_NAME } = process.env;
 
-dotenv.config()
+
 cloudinary.config({
     cloud_name: C_NAME,
     api_key:API_KEY,
