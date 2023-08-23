95be8281d127d77397b985dc47feb31ea742d88a
Juan <juampi.parzybal@gmail.com>
2023-08-22 22:58:30 -0300

    changes for use server deployed

--- Client/src/utils/fetcherPut.js
+++ Client/src/utils/fetcherPut.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL= "http://localhost:3001"
+const BASE_URL= "https://mere-hands-production.up.railway.app"
 
 
 export const updateProductFunction= async (endpoint, updateProduct)=>{
