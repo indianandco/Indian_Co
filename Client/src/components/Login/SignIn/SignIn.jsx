8cf4166dd550ba01493321de72682cc259f0e923
Juan <juampi.parzybal@gmail.com>
2023-09-07 12:40:48 -0300

    fix(errors): general changes

--- Client/src/components/Login/SignIn/SignIn.jsx
+++ Client/src/components/Login/SignIn/SignIn.jsx
@@ -129,8 +129,6 @@ function SignIn({ onClick }) {
         setShowLogin(false);
     };
 
-
-
     useEffect(() => {
         validation({ ...loginForm })
     }, [loginForm])
--- Client/src/services/ProductContext.jsx
+++ Client/src/services/ProductContext.jsx
@@ -2,7 +2,7 @@ import { createContext, useState } from "react";
 import { postProductFunction } from "../utils/fetcherPost"
 import { updateProductFunction } from "../utils/fetcherPut"
 import { fetcher } from "../utils/fetcherGet";
-import{ deleteProduct} from "../utils/fetcherDelete"
+import { deleteProduct } from "../utils/fetcherDelete"
 
 export const ProductContext = createContext()
 
@@ -19,7 +19,7 @@ export const ProductProvider = ({ children }) => {
 
 
 
-    
+
 
     const postProduct = async (product) => {
         const responseBack = await postProductFunction('/adminDashboard/products/create', product)
@@ -28,11 +28,11 @@ export const ProductProvider = ({ children }) => {
     }
 
     const getAllProducts = async () => {
-        const response =await (fetcher(`/products`))
+        const response = await (fetcher(`/products`))
         setAllProducts(response.payload);
         setDisplayedProducts(response.payload);
-        setProductStorage(response.payload) 
- 
+        setProductStorage(response.payload)
+
     };
 
     const getDetailProducts = async (id) => {
@@ -47,43 +47,45 @@ export const ProductProvider = ({ children }) => {
             return;
         }
         const response = await fetcher(`/products/search?title=${title}`);
-        if(response.error){
-            setError(response.error); 
-        }else{
+        if (response.error) {
+            setError(response.error);
+        } else {
             setDisplayedProducts(response);
         }
     };
 
     const findProductStorage = (title) => {
         const newProducts = allProducts
-    
-        const filteredProducts = newProducts.filter(product => 
+
+        const filteredProducts = newProducts.filter(product =>
             product.title.toLowerCase().includes(title.toLowerCase())
         );
-    
+
         setProductStorage(filteredProducts);
     };
-    
-    
+
+
     const filterByCategory = async (category) => {
         const response = await fetcher(`/products/category/${category}`);
         setProductStorage(response);
     };
 
-    const sortProducts=async(prop)=>{
+    const sortProducts = async (prop) => {
         const response = await fetcher(`/products?sort=${prop}`)
         setProductStorage(response.payload)
     }
 
-    const offerProducts=async(prop)=>{
-        if(prop ==="true"){
-            const filtered= allProducts.filter((product)=>{
-                return product.offer === true})
-                setProductStorage(filtered)
-        }else if (prop ==="false"){
-            const filtered= allProducts.filter((product)=>{
-                return product.offer === false})
-                setProductStorage(filtered)
+    const offerProducts = async (prop) => {
+        if (prop === "true") {
+            const filtered = allProducts.filter((product) => {
+                return product.offer === true
+            })
+            setProductStorage(filtered)
+        } else if (prop === "false") {
+            const filtered = allProducts.filter((product) => {
+                return product.offer === false
+            })
+            setProductStorage(filtered)
         }
     }
 
@@ -101,13 +103,14 @@ export const ProductProvider = ({ children }) => {
     const resetPagination = () => {
         setPagActive(1);
     }
-   
-const deleteProductFn= async(id)=>{
-    const response = await deleteProduct(`/adminDashboard/products/delete/${id}`)
-}
+
+    const deleteProductFn = async (id) => {
+        const response = await deleteProduct(`/adminDashboard/products/delete/${id}`)
+        return response;
+    }
 
     return (
-        <ProductContext.Provider value={{deleteProductFn,offerProducts,pagActive, setPagActive,resetPagination,sortProducts,clearSearch,error, clearError,displayedProducts, getAllProducts, findProduct, findProductStorage,productStorage, filterByCategory, response, allProducts, detailProducts, getDetailProducts, postProduct, updateProduct }}>
+        <ProductContext.Provider value={{ deleteProductFn, offerProducts, pagActive, setPagActive, resetPagination, sortProducts, clearSearch, error, clearError, displayedProducts, getAllProducts, findProduct, findProductStorage, productStorage, filterByCategory, response, allProducts, detailProducts, getDetailProducts, postProduct, updateProduct }}>
             {children}
         </ ProductContext.Provider >
     )
