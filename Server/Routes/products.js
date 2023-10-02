9185d4501a353c40c4c7539ecf553a6b6b2cda09
Juan <juampi.parzybal@gmail.com>
2023-10-02 12:10:07 -0300

    @update/products

--- Server/Routes/products.js
+++ Server/Routes/products.js
@@ -6,7 +6,7 @@ const { getProductByIdHandler } = require("../Handlers/get/getProductByIdHandler
 const { getProductByCategoryHandler } = require("../Handlers/get/getProductByCategoryHandler");
 const { getProductByTitleHandler } = require("../Handlers/get/getProductByTitleHandler");
 
-router.get("/", getProductsHandler);
+router.get("/:products", getProductsHandler);
 router.get("/category/:category", getProductByCategoryHandler);
 router.get("/search", getProductByTitleHandler);
 router.get("/:id", getProductByIdHandler);
